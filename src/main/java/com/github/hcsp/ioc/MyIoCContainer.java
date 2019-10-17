ab402e418afcf95954ee4e362b9953333dd96eeb
周武栋 <wzhou1@xiaohongshu.com>
2019-10-17 15:16:37 +0800

    simple ioc contaioner

--- src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
+++ src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
@@ -1,6 +1,17 @@
 package com.github.hcsp.ioc;
 
+import java.util.Properties;
+import java.util.Set;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+
 public class MyIoCContainer {
+
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
     // 实现一个简单的IoC容器，使得：
     // 1. 从beans.properties里加载bean定义
     // 2. 自动扫描bean中的@Autowired注解并完成依赖注入
@@ -12,10 +23,29 @@ public class MyIoCContainer {
     }
 
     // 启动该容器
-    public void start() {}
+    public void start() {
+        context.refresh();
+        registerAllBeansFromProperties("beans.properties", context);
+    }
 
     // 从容器中获取一个bean
     public Object getBean(String beanName) {
-        return null;
+        return context.getBean(beanName);
     }
+
+    public static void registerAllBeansFromProperties(String fileName, AnnotationConfigApplicationContext context) {
+        try {
+            Properties properties = PropertiesLoaderUtils.loadAllProperties(fileName);
+            Set<Object> objects = properties.keySet();
+            for (Object key : objects) {
+                String className = (String) properties.get(key);
+                Class<?> clazz = Class.forName(className);
+                BeanDefinition beanDefinition = new RootBeanDefinition(clazz);
+                context.registerBeanDefinition(key.toString(), beanDefinition);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
