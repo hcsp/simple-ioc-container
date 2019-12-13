a9c7ebdbb4dbb0ee9098307143f089588977708d
dflk <904885508@qq.com>
2019-12-14 00:09:04 +0800

    catchYou

--- src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
+++ src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
@@ -1,10 +1,23 @@
 package com.github.hcsp.ioc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MyIoCContainer {
+    private Map<String, Object> beans = new HashMap<>();
+
     // 实现一个简单的IoC容器，使得：
     // 1. 从beans.properties里加载bean定义
     // 2. 自动扫描bean中的@Autowired注解并完成依赖注入
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         MyIoCContainer container = new MyIoCContainer();
         container.start();
         OrderService orderService = (OrderService) container.getBean("orderService");
@@ -12,10 +25,46 @@ public class MyIoCContainer {
     }
 
     // 启动该容器
-    public void start() {}
+    public void start() {
+        Properties properties = new Properties();
+        try {
+            properties.load(MyIoCContainer.class.getResourceAsStream("/beans.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        properties.forEach((beanName, beanClass) -> {
+            try {
+                Class<?> klass = Class.forName((String) beanClass);
+                Object beanInstance = klass.getConstructor().newInstance();
+                beans.put((String) beanName, beanInstance);
+            } catch (Exception e) {
+                throw new RuntimeException();
+            }
+        });
+
+        beans.forEach((beanName, beanInstance) -> dependencyInject(beanName, beanInstance, beans));
+    }
+
+    private void dependencyInject(String beanName, Object beanInstance, Map<String, Object> beans) {
+        List<Field> fieldsToBeAutoWired = Stream.of(beanInstance.getClass().getDeclaredFields())
+                .filter(field -> field.getAnnotation(Autowired.class) != null)
+                .collect(Collectors.toList());
+
+        fieldsToBeAutoWired.forEach(field -> {
+            String fieldName = field.getName();
+            Object dependencyBensInstance = beans.get(fieldName);
+            field.setAccessible(true);
+            try {
+                field.set(beanInstance, dependencyBensInstance);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException();
+            }
+        });
+    }
 
     // 从容器中获取一个bean
     public Object getBean(String beanName) {
-        return null;
+        return beans.get(beanName);
     }
 }
