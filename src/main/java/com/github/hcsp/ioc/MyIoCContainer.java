b18f273f11749b061a03b5d8866898171d51364a
steven-wan <951520698@qq.com>
2020-01-10 17:42:57 +0800

    遇到一个反射错误，没有想通原因

--- src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
+++ src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
@@ -1,6 +1,21 @@
 package com.github.hcsp.ioc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MyIoCContainer {
+    private HashMap map = new HashMap();
+    Properties properties;
+
     // 实现一个简单的IoC容器，使得：
     // 1. 从beans.properties里加载bean定义
     // 2. 自动扫描bean中的@Autowired注解并完成依赖注入
@@ -12,10 +27,58 @@ public class MyIoCContainer {
     }
 
     // 启动该容器
-    public void start() {}
+    public void start() {
+        try {
+            properties = PropertiesLoaderUtils.loadProperties(new InputStreamResource(getClass().getClassLoader().getResourceAsStream("beans.properties")));
+            properties.entrySet().forEach(entry -> {
+                if (!map.containsKey(entry.getKey())) {
+                    try {
+                        map.put(entry.getKey(), injectionAccountValue((String) entry.getValue()));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private Object injectionAccountValue(String className) throws IllegalAccessException, InstantiationException {
+        Class kCla = null;
+        try {
+            kCla = Class.forName(className);
+            List<Field> fields = getFields(kCla);
+            injectionFields(fields);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return kCla.newInstance();
+    }
+
+    private List<Field> getFields(Class kCla) {
+        return Stream.of(kCla.getDeclaredFields()).filter(field -> field.getAnnotation(Autowired.class) != null).collect(Collectors.toList());
+    }
+
+    private void injectionFields(List<Field> fields) {
+        fields.stream().forEach(field -> {
+            String fileName = field.getName();
+            String fieldClassName = properties.getProperty(fileName);
+            try {
+                Class<?> aClass = Class.forName(fieldClassName);
+                injectionFields(getFields(aClass));
+                field.setAccessible(true);
+                field.set(fileName,aClass.newInstance());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 
     // 从容器中获取一个bean
     public Object getBean(String beanName) {
-        return null;
+        return map.get(beanName);
     }
 }
