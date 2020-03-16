4cf85267a8c4253d2b694ce8744a66a63932e0b7
KisetsuLee <42855253+KisetsuLee@users.noreply.github.com>
2020-03-16 22:53:39 +0800

    简易版本ioc (#42)
    简易版本ioc (#42)

--- src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
+++ src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
@@ -1,6 +1,19 @@
 package com.github.hcsp.ioc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MyIoCContainer {
+    private ConcurrentMap<String, Object> container = new ConcurrentHashMap<>();
+
     // 实现一个简单的IoC容器，使得：
     // 1. 从beans.properties里加载bean定义
     // 2. 自动扫描bean中的@Autowired注解并完成依赖注入
@@ -11,11 +24,46 @@ public class MyIoCContainer {
         orderService.createOrder();
     }
 
+    private void dependenceInject(Object bean) {
+        List<Field> autoWiredFiled = Stream.of(bean.getClass().getDeclaredFields())
+                .filter(field -> field.getAnnotation(Autowired.class) != null)
+                .collect(Collectors.toList());
+        autoWiredFiled.forEach(field -> {
+            try {
+                field.setAccessible(true);
+                field.set(bean, container.get(field.getName()));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private void readConfigAndInitialBeans(String configPath) throws IOException {
+        Properties properties = new Properties();
+        properties.load(MyIoCContainer.class.getResourceAsStream(configPath));
+        properties.forEach((beanName, beanClass) -> {
+            try {
+                Class<?> clazz = Class.forName((String) beanClass);
+                Object instance = clazz.getConstructor().newInstance();
+                container.put((String) beanName, instance);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
     // 启动该容器
-    public void start() {}
+    public void start() {
+        try {
+            readConfigAndInitialBeans("/beans.properties");
+            container.values().forEach(this::dependenceInject);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     // 从容器中获取一个bean
     public Object getBean(String beanName) {
-        return null;
+        return container.get(beanName);
     }
 }
