a0c04c5bb95adc11a7713db03039ee5e5111c15f
linkai.huang <>
2020-04-06 21:50:56 +0800

    一个简单的IoC容器，userService回滚

--- src/main/java/com/github/hcsp/ioc/UserService.java
+++ src/main/java/com/github/hcsp/ioc/UserService.java
@@ -3,8 +3,7 @@ package com.github.hcsp.ioc;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserService {
-    @Autowired
-    private UserDao userDao;
+    @Autowired private UserDao userDao;
 
     public User getCurrentLoginUser() {
         return userDao.getUserById(1);
