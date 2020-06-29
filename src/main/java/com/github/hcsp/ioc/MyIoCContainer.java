28c3f9cb40b75d0ecb688077897d7ebf8a0d7649
Sunpeng526 <215860865@qq.com>
2020-06-29 02:38:37 -0700

    ioc simple code

--- src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
+++ src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
@@ -2,9 +2,16 @@ package com.github.hcsp.ioc;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class MyIoCContainer {
     /*
@@ -18,41 +25,63 @@ public class MyIoCContainer {
         orderService.createOrder();
     }
 
-    private Map<String, Object> beans;
+    public Map<String, Object> beans = new HashMap<>();
+
     /*
      * 启动该容器
      */
     public void start() {
         Properties properties = new Properties();
-        FileInputStream propertiesInputStream;
+        FileInputStream propertiesInputStream = null;
         try {
             propertiesInputStream = new FileInputStream("./src/main/resources/beans.properties");
             properties.load(propertiesInputStream);
-            properties.forEach((k,v)->beans.add(k,Class.forName(p).getDeclaredConstructor().newInstance()));
-            propertiesInputStream.close();
-        } catch (IOException | ClassNotFoundException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+            properties.forEach((k, v) -> {
+                try {
+                    beans.put((String) k, Class.forName((String) v).getConstructor().newInstance());
+                } catch (IllegalArgumentException | SecurityException | ClassNotFoundException | InstantiationException
+                        | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
+        beans.forEach((beanName, bean) -> iocInject(beanName, bean, beans));
+    }
+
+    /**
+     * @param beanName
+     * @param bean
+     * @param beans
+     */
+    private void iocInject(String beanName, Object bean, Map<String, Object> beans) {
+        List<Field> fields = Stream.of(bean.getClass().getDeclaredFields())
+                .filter(field -> field.getAnnotation(Autowired.class) != null).collect(Collectors.toList());
+
+        fields.forEach(field -> {
+            String filedName = field.getName();
+            Object beanInstance = beans.get(filedName);
+            field.setAccessible(true);
+            try {
+                field.set(bean, beanInstance);
+            } catch (IllegalArgumentException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        });
     }
 
     /*
      * 从容器中获取一个bean
      */
     public Object getBean(String beanName) {
-        Properties beans = new Properties();
-        Object result = null;
-        FileInputStream properties;
-        try {
-            properties = new FileInputStream("./src/main/resources/beans.properties");
-            beans.load(properties);
-            String clazzString = beans.getProperty(beanName);
-            result = Class.forName(clazzString).getDeclaredConstructor().newInstance();
-            properties.close();
-        } catch (IOException | ClassNotFoundException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return result;
+        return beans.get(beanName);
     }
 }
