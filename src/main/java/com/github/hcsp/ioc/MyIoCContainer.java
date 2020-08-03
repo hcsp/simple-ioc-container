5e95f1955a068095d68cd47d07ee64caf57c94be
McDul <907420318@qq.com>
2020-08-03 17:07:36 +0800

    Spring真有趣~ (#76)
    Spring真有趣~ (#76)

--- src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
+++ src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
@@ -1,6 +1,26 @@
 package com.github.hcsp.ioc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.Set;
+
 public class MyIoCContainer {
+    private Properties properties;
+    private HashMap<String, Object> beans;
+
+    public MyIoCContainer() {
+        properties = new Properties();
+        try {
+            properties.load(ClassLoader.getSystemResourceAsStream("beans.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     // 实现一个简单的IoC容器，使得：
     // 1. 从beans.properties里加载bean定义
     // 2. 自动扫描bean中的@Autowired注解并完成依赖注入
@@ -12,10 +32,40 @@ public class MyIoCContainer {
     }
 
     // 启动该容器
-    public void start() {}
+    public void start() {
+        beans = new HashMap<>();
+        Set<Object> keys = properties.keySet();
+        for (Object key : keys) {
+            try {
+                Class<?> aClass = getClass().getClassLoader().loadClass((String) properties.get(key));
+                Object newInstance = aClass.newInstance();
+                beans.put(key.toString(), newInstance);
+            } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        scanNotefromBeans();
+    }
+
+    private void scanNotefromBeans() {
+        beans.forEach((name, bean) -> {
+            Arrays.stream(bean.getClass().getDeclaredFields())
+                    .filter(field -> field.getAnnotation(Autowired.class) != null)
+                    .forEach(field -> {
+                        if (beans.get(field.getName()) != null) {
+                            field.setAccessible(true);
+                            try {
+                                field.set(bean, beans.get(field.getName()));
+                            } catch (IllegalAccessException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    });
+        });
+    }
 
     // 从容器中获取一个bean
     public Object getBean(String beanName) {
-        return null;
+        return beans.get(beanName);
     }
 }
