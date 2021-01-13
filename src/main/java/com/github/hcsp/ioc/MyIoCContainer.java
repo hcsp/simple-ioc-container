9031f89864e53f341dd9ecfe2ab5cc4d63d67417
qipengWei <webqipeng@163.com>
2021-01-13 23:18:26 +0800

    CI

--- src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
+++ src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
@@ -17,7 +17,7 @@ public class MyIoCContainer {
     // 实现一个简单的IoC容器，使得：
     // 1. 从beans.properties里加载bean定义
     // 2. 自动扫描bean中的@Autowired注解并完成依赖注入
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         MyIoCContainer container = new MyIoCContainer();
         container.start();
         OrderService orderService = null;
