bc4ce013df01063f513023d698e37075a683121b
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2021-08-06 18:05:16 +0800

    feat: complete MyIoCContainer (#103)
    feat: complete MyIoCContainer (#103)

--- src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
+++ src/main/java/com/github/hcsp/ioc/MyIoCContainer.java
@@ -1,6 +1,19 @@
 package com.github.hcsp.ioc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Stream;
+
 public class MyIoCContainer {
+    private final Properties properties = new Properties();
+    private final Map<String, Object> dependencyMap = new HashMap<>();
+
     // 实现一个简单的IoC容器，使得：
     // 1. 从beans.properties里加载bean定义
     // 2. 自动扫描bean中的@Autowired注解并完成依赖注入
@@ -12,10 +25,50 @@ public class MyIoCContainer {
     }
 
     // 启动该容器
-    public void start() {}
+    public void start() {
+        try {
+            String beanFileName = "beans.properties";
+            InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(beanFileName);
+            properties.load(resourceAsStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     // 从容器中获取一个bean
     public Object getBean(String beanName) {
-        return null;
+        try {
+            if (dependencyMap.get(beanName) != null) {
+                return dependencyMap.get(beanName);
+            }
+            Class<?> aClass = Class.forName(properties.getProperty(beanName));
+            Object beanObject = aClass.getConstructor().newInstance();
+            wireDependencies(aClass, beanObject);
+            dependencyMap.put(beanName, beanObject);
+            return beanObject;
+        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void wireDependencies(Class<?> aClass, Object beanObject) {
+        Stream.of(aClass.getDeclaredFields())
+                .filter(field -> field.getAnnotation(Autowired.class) != null)
+                .forEach(field -> {
+                    try {
+                        String typeName = field.getGenericType().getTypeName();
+                        Object instance = dependencyMap.get(typeName);
+                        if (instance == null) {
+                            Class<?> fieldClass = Class.forName(typeName);
+                            instance = fieldClass.getConstructor().newInstance();
+                            wireDependencies(fieldClass, instance);
+                        }
+                        dependencyMap.put(typeName, instance);
+                        field.setAccessible(true);
+                        field.set(beanObject, instance);
+                    } catch (InstantiationException | ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
