58af7e785e836153e3dec7fcc5bf38b8ab8b0235
bowen-wu <13793248162@163.com>
2021-08-07 15:06:57 +0800

    fix: fixed getBean method

--- src/main/java/com/github/hcsp/ioc/OrderService.java
+++ src/main/java/com/github/hcsp/ioc/OrderService.java
@@ -3,10 +3,10 @@ package com.github.hcsp.ioc;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class OrderService {
-    @Autowired private OrderDao orderDao;
+    @Autowired private OrderDao orderDaox;
     @Autowired private UserService userService;
 
     public void createOrder() {
-        orderDao.createOrder(userService.getCurrentLoginUser());
+        orderDaox.createOrder(userService.getCurrentLoginUser());
     }
 }
